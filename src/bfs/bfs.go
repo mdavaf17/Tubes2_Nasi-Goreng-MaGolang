8cbfba02627f1b2f897cd348f49ad083cdeed290
Ariel-HS <arielherfrison@gmail.com>
2024-04-21 12:34:59 +0700

    Update bfs.go
    now uses tree

--- src/bfs/bfs.go
+++ src/bfs/bfs.go
@@ -2,13 +2,12 @@ package bfs
 
 import (
 	"fmt"
-	"os"
 	"regexp"
 	"slices"
 
 	"github.com/dominikbraun/graph"
-	"github.com/dominikbraun/graph/draw"
 	"github.com/gocolly/colly"
+	"github.com/kingledion/go-tools/tree"
 )
 
 func Main(startURL, goalURL string) *graph.Graph[string, string] {
@@ -21,6 +20,7 @@ func Main(startURL, goalURL string) *graph.Graph[string, string] {
 
 	// Initiate queue to store to-be visited links
 	queue := []string{}
+	queue_id := []uint{}
 
 	// Var to store current link
 	var current_link string
@@ -28,8 +28,10 @@ func Main(startURL, goalURL string) *graph.Graph[string, string] {
 	// Initiate bool to determine if solution is reached
 	found := false
 
-	// Initiate graph
-	g := graph.New(graph.StringHash, graph.Directed())
+	// Initiate tree and id for tree
+	t := tree.Empty[string]()
+	var parent_id uint
+	var current_id uint = 1
 
 	c := colly.NewCollector(
 		colly.URLFilters(
@@ -48,19 +50,12 @@ func Main(startURL, goalURL string) *graph.Graph[string, string] {
 			// Append link to array of to-be visited links
 			absolute_link := e.Request.AbsoluteURL(link)
 			queue = append(queue, absolute_link)
+			queue_id = append(queue_id, current_id)
 
-			_ = g.AddVertex(absolute_link)
+			t.Add(current_id, parent_id, absolute_link)
 
-			_ = g.AddEdge(current_link, absolute_link)
-
+			current_id++
 		}
-		// Visit link found on page
-		// Only those links are visited which are matched by  any of the URLFilter regexps
-		// if len(queue) > 0 {
-		// 	visit := queue[0]
-		// 	queue = queue[1:]
-		// 	c.Visit(e.Request.AbsoluteURL(visit))
-		// }
 	})
 
 	// Before making a request print "Visiting ..."
@@ -68,31 +63,52 @@ func Main(startURL, goalURL string) *graph.Graph[string, string] {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
+	// Find path to goal URL
+	findPath := func(id uint) []string {
+		path := []string{}
+
+		n, _ := t.Find(id)
+		for !(n.GetID() == 0) {
+			path = append(path, n.GetData())
+			n = n.GetParent()
+		}
+
+		path = append(path, n.GetData())
+
+		return path
+	}
+
 	// Start scraping on start_url
+	t.Add(0, 0, startURL)
 	queue = append(queue, startURL)
-	_ = g.AddVertex(startURL)
+	queue_id = append(queue_id, 0)
 	for (!found) && (len(queue) > 0) {
 		current_link = queue[0]
+		parent_id = queue_id[0]
 		if current_link == goalURL {
 			found = true
 			fmt.Println("Link Found! " + current_link)
 
 		} else {
 			queue = queue[1:]
+			queue_id = queue_id[1:]
 			visited = append(visited, current_link)
 			c.Visit(current_link)
 		}
 	}
 
-	fmt.Println(startURL)
-	file, _ := os.Create("./mygraph.gv")
-	_ = draw.DOT(g, file)
+	// Get path to goal URL
+	path := findPath(parent_id)
 
-	// find path from graph
-	path_all, _ := graph.AllPathsBetween(g, startURL, goalURL)
-	path, _ := graph.ShortestPath(g, startURL, goalURL)
-	fmt.Println(path_all)
-	fmt.Println(path)
+	// Initiate graph
+	g := graph.New(graph.StringHash, graph.Directed())
+
+	// Add path to graph
+	for i := len(path) - 1; i > 0; i-- {
+		_ = g.AddVertex(path[i])
+		_ = g.AddVertex(path[i-1])
+		_ = g.AddEdge(path[i], path[i-1])
+	}
 
 	return &g
 }
